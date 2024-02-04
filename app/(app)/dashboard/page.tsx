c69248086e0aac69801c7cee00a5f5b9cd654b7f
berkay can <berkaycanboga@gmail.com>
2024-02-04 10:10:31 +0300

    chore: refactored DashboardPage component and integrated with user data

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,12 +1,31 @@
 import React from "react";
-import UserDashboard from "../../../components/Dashboard/UserDashboard";
+import Dashboard from "../../../components/Dashboard/Dashboard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../api/auth/[...nextauth]/options";
+import {
+  getUserPostsController,
+  getUserFollowingPostsController,
+} from "../../../controllers/PostController";
 
-const Page = () => {
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  const userId = session?.user.id as number;
+  const username = session?.user.username as string;
+  const fullName = session?.user.username as string;
+
+  const userPosts = await getUserPostsController(userId);
+  const userFollowingPosts = await getUserFollowingPostsController(userId);
+
+  const posts = [...userPosts, ...userFollowingPosts];
+
   return (
-    <div className="flex">
-      <UserDashboard />
-    </div>
+    <Dashboard
+      userId={userId}
+      posts={posts}
+      username={username}
+      fullName={fullName}
+    />
   );
 };
 
-export default Page;
+export default DashboardPage;
