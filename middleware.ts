c8abd2e602405c3e477a1de18023e6e823014f8f
berkay can <berkaycanboga@gmail.com>
2024-02-04 10:48:19 +0300

    feat: add initial middleware configuration

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { decodeToken } from "./utils/decode";
+
+export { default } from "next-auth/middleware";
+
+export async function middleware(req: NextRequest) {
+  const decodedToken = await decodeToken(req);
+
+  if (!decodedToken) {
+    return NextResponse.redirect(new URL("/signin", req.url));
+  }
+
+  const currentUser = decodedToken!.id;
+  const targetUserId = parseInt(req.url?.split("/")[5]);
+
+  if (targetUserId && targetUserId !== currentUser)
+    return NextResponse.json(
+      { error: "Forbidden: Operation on behalf of another user" },
+      { status: 403 },
+    );
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/api/user/:path*", "/dashboard/:path*"],
+};
