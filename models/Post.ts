c1cfb6929758ba83c1c43c25fb88f17afbf4be37
berkay can <berkaycanboga@gmail.com>
2024-02-04 10:30:37 +0300

    chore: updated Post interface with additional properties and adjustments

--- models/Post.ts
+++ models/Post.ts
@@ -1,7 +1,11 @@
 import { User } from "./User";
 
 export interface Post {
+  id?: number;
   content: string;
   author?: User;
   authorId: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+  isPlaceholder?: boolean;
 }
