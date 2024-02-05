37a2b7194f78e87098fd3b0d63e99521c55b27ba
berkay can <berkaycanboga@gmail.com>
2024-02-05 09:44:50 +0300

    chore: removed unnecessary user-related functions

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -1,20 +1,10 @@
-import { LoginUser, SignUpUser } from "../models/User";
-import { createUser, findUser } from "../services/AuthService";
+import { SignUpUser } from "../models/User";
+import { createUser } from "../services/AuthService";
 
 export const signUp = async (userData: SignUpUser) => {
   const { password, ...rest } = userData;
 
   const user = await createUser({ ...rest, password });
-
-  return user;
-};
-
-export const signIn = async (credentials: LoginUser) => {
-  const user = await findUser(credentials);
-
-  if (!user) {
-    throw new Error("Authentication failed: User not found");
-  }
 
   return user;
 };
--- services/AuthService.ts
+++ services/AuthService.ts
@@ -49,35 +49,3 @@ export const createUser = async (userData: SignUpUser) => {
 
   return user;
 };
-
-export const findUser = async (credentials: LoginUser) => {
-  const { identifier, password } = credentials;
-
-  const validation: SignInValidationResult = validateSignIn({
-    identifier,
-    password,
-  });
-
-  if (validation.errors && Object.keys(validation.errors).length > 0) {
-    const firstErrorKey = Object.keys(validation.errors)[0];
-    throw new Error(validation.errors[firstErrorKey]);
-  }
-
-  const { identifierType } = validation;
-
-  const user = await prisma.user.findFirst({
-    where: {
-      OR: [
-        { username: identifierType === "username" ? identifier : undefined },
-        { email: identifierType === "email" ? identifier : undefined },
-        { phone: identifierType === "phone" ? identifier : undefined },
-      ],
-    },
-  });
-
-  if (!user) {
-    throw new Error("User not found");
-  }
-
-  return user;
-};
