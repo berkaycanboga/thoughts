9942973da2e2be03a892e0b0944f6e790f83b733
berkay can <berkaycanboga@gmail.com>
2024-02-05 05:39:50 +0300

    feat: integrated Axios for signUpApi request

--- utils/api/signUpApi.ts
+++ utils/api/signUpApi.ts
@@ -1,24 +1,20 @@
+import axios from "axios";
 import { SignUpUser } from "../../models/User";
 import { signInApi } from "./signInApi";
 
 export const signUpApi = async (data: SignUpUser) => {
   try {
-    const response = await fetch("/api/auth/signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    const response = await axios.post("/api/auth/signup", data);
 
-    if (response.ok) {
+    if (response.status === 200) {
       await signInApi(data);
       return { success: true };
     } else {
-      const errorData = await response.json();
+      const errorData = response.data;
       return { error: errorData.message || "Error signing up" };
     }
   } catch (error) {
+    console.error("Error signing up:", error);
     return { error: "Error signing up" };
   }
 };
