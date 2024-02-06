7d2d5f818942ed6c8408cd83e02d6694a48050e7
berkay can <berkaycanboga@gmail.com>
2024-02-06 18:47:49 +0300

    feat: implemented PostContainer component for consistent post layout

---
+++ components/Common/PostContainer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface PostContainerProps {
+  children: React.ReactNode;
+}
+
+const PostContainer = ({ children }: PostContainerProps) => {
+  return (
+    <div className="flex justify-center">
+      <div className="border border-gray-200 rounded-md p-4 w-full max-w-xl">
+        <div className="container mx-auto mt-8">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default PostContainer;
