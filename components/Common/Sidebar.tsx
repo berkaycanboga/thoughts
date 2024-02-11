8390915cc8634d52328929f987b01e906d13c090
berkay can <berkaycanboga@gmail.com>
2024-02-11 23:06:43 +0300

    feat: added sidebar search

--- components/Common/Sidebar.tsx
+++ components/Common/Sidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// Sidebar.tsx
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -14,6 +15,14 @@ import {
   BsPersonFill,
 } from "react-icons/bs";
 
+import Search from "../Search/Search";
+
+interface RenderLinkProps {
+  path: string;
+  icon: React.ComponentType<{ className: string }>;
+  label: string;
+}
+
 const Sidebar = () => {
   const pathname = usePathname();
   const currentPath = pathname;
@@ -21,6 +30,7 @@ const Sidebar = () => {
   const isIconFilled = (path: string) => currentPath.startsWith(path);
 
   const [windowWidth, setWindowWidth] = useState(0);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   const handleResize = () => {
     setWindowWidth(window.innerWidth);
@@ -38,115 +48,90 @@ const Sidebar = () => {
 
   const showIconsOnly = windowWidth <= 1000;
 
-  return (
-    <div
-      className={`fixed left-0 top-0 border-r border-gray-300 h-full flex flex-col items-center justify-start bg-gray-100 shadow-md p-4 ${showIconsOnly ? "" : "sm:pr-36"}`}
+  const renderLink = ({ path, icon, label }: RenderLinkProps) => (
+    <Link
+      href={path}
+      className="flex items-center p-3 rounded-md hover:bg-gray-100 w-full"
     >
-      <div className="mb-4 flex items-center">
-        <Image
-          src={showIconsOnly ? "/favicon.ico" : "/logo.svg"}
-          alt="Logo"
-          width={showIconsOnly ? 24 : 120}
-          height={showIconsOnly ? 24 : 120}
-          priority={true}
-        />
-      </div>
-      <ul className="space-y-4 w-full">
-        <li>
-          <Link
-            href="/dashboard"
-            className="flex items-center p-3 rounded-md hover:bg-gray-100 w-full"
-          >
-            {isIconFilled("/dashboard") ? (
-              <BsHouseFill
-                className={`icon ${showIconsOnly ? "" : "mr-3"} text-cyan-500 text-2xl`}
-              />
-            ) : (
-              <BsHouse
-                className={`icon ${showIconsOnly ? "" : "mr-3"} text-gray-500 hover:text-cyan-500 text-2xl`}
-              />
-            )}
-            {!showIconsOnly && (
-              <span
-                className={`hidden sm:inline-block text-lg ${
-                  isIconFilled("/") ? "font-bold" : ""
-                }`}
-              >
-                Home
-              </span>
-            )}
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/search"
-            className="flex items-center p-3 rounded-md hover:bg-gray-100 w-full"
-          >
-            <BsSearch
-              className={`icon ${showIconsOnly ? "" : "mr-3"} text-gray-500 hover:text-cyan-500 text-2xl`}
+      {isIconFilled(path)
+        ? React.createElement(icon, {
+            className: `icon ${!showIconsOnly && "mr-3"} text-cyan-500 text-2xl`,
+          })
+        : React.createElement(icon, {
+            className: `icon ${!showIconsOnly && "mr-3"} text-gray-500 hover:text-cyan-500 text-2xl`,
+          })}
+      {!showIconsOnly && (
+        <span
+          className={`hidden sm:inline-block text-lg ${
+            isIconFilled(path) ? "font-bold" : ""
+          }`}
+        >
+          {label}
+        </span>
+      )}
+    </Link>
+  );
+
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
+  return (
+    <div className="flex relative">
+      <div
+        className={`fixed left-0 top-0 border-r border-gray-300 h-full flex flex-col bg-gray-100 shadow-md p-4 ${showIconsOnly ? "" : "min-w-80"}`}
+      >
+        <ul className="space-y-4 w-full">
+          <div className="mb-4 p-3">
+            <Image
+              src={showIconsOnly ? "/favicon.ico" : "/logo.svg"}
+              alt="Logo"
+              width={showIconsOnly ? 24 : 120}
+              height={showIconsOnly ? 24 : 120}
+              priority={true}
             />
-            {!showIconsOnly && (
-              <span
-                className={`hidden sm:inline-block text-lg ${
-                  isIconFilled("/search") ? "font-bold" : ""
-                }`}
-              >
-                Search
-              </span>
-            )}
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/create"
-            className="flex items-center p-3 rounded-md hover:bg-gray-100 w-full"
-          >
-            {isIconFilled("/create") ? (
-              <BsPencilFill
-                className={`icon ${showIconsOnly ? "" : "mr-3"} text-cyan-500 text-2xl`}
-              />
-            ) : (
-              <BsPencil
-                className={`icon ${showIconsOnly ? "" : "mr-3"} text-gray-500 hover:text-cyan-500 text-2xl`}
-              />
-            )}
-            {!showIconsOnly && (
-              <span
-                className={`hidden sm:inline-block text-lg ${
-                  isIconFilled("/create") ? "font-bold" : ""
-                }`}
-              >
-                Create
-              </span>
-            )}
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/profile"
-            className="flex items-center p-3 rounded-md hover:bg-gray-100 w-full"
-          >
-            {isIconFilled("/profile") ? (
-              <BsPersonFill
-                className={`icon ${showIconsOnly ? "" : "mr-3"} text-cyan-500 text-2xl`}
-              />
-            ) : (
-              <BsPerson
-                className={`icon ${showIconsOnly ? "" : "mr-3"} text-gray-500 hover:text-cyan-500 text-2xl`}
-              />
-            )}
-            {!showIconsOnly && (
-              <span
-                className={`hidden sm:inline-block text-lg ${
-                  isIconFilled("/profile") ? "font-bold" : ""
-                }`}
-              >
-                Profile
-              </span>
-            )}
-          </Link>
-        </li>
-      </ul>
+          </div>
+          <li>
+            {renderLink({
+              path: "/dashboard",
+              icon: isIconFilled("/dashboard") ? BsHouseFill : BsHouse,
+              label: "Home",
+            })}
+          </li>
+          <li>
+            <button
+              className="flex items-center p-3 rounded-md hover:bg-gray-100 w-full"
+              onClick={toggleSearch}
+            >
+              {React.createElement(BsSearch, {
+                className: `icon ${!showIconsOnly && "mr-3"} text-gray-500 hover:text-cyan-500 text-2xl`,
+              })}
+              {!showIconsOnly && (
+                <span className={`hidden sm:inline-block text-lg`}>Search</span>
+              )}
+            </button>
+          </li>
+          <li>
+            {renderLink({
+              path: "/create",
+              icon: isIconFilled("/create") ? BsPencilFill : BsPencil,
+              label: "Create",
+            })}
+          </li>
+          <li>
+            {renderLink({
+              path: "/profile",
+              icon: isIconFilled("/profile") ? BsPersonFill : BsPerson,
+              label: "Profile",
+            })}
+          </li>
+        </ul>
+      </div>
+      {isSearchOpen && (
+        <div className="fixed top-0 border-r border-gray-300 h-full flex flex-col bg-gray-100 shadow-md p-4 min-w-80">
+          <Search onClose={toggleSearch} />
+        </div>
+      )}
     </div>
   );
 };
