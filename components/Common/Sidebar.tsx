3e9b91efc477f0820fc17b2f04acc83ee3a3c76f
berkay can <berkaycanboga@gmail.com>
2024-02-13 13:38:27 +0300

    feat: added user profile navigation into sidebar

--- components/Common/Sidebar.tsx
+++ components/Common/Sidebar.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { getSession } from "next-auth/react";
 import React, { useState, useEffect } from "react";
 import {
   BsHouse,
@@ -108,57 +109,73 @@ const SidebarContent = ({
   renderLink: (props: LinkProps) => JSX.Element;
   isIconFilled: (path: string) => boolean;
   toggleSearch: () => void;
-}) => (
-  <div
-    className={`fixed left-0 top-0 border-r border-gray-300 h-full flex flex-col bg-gray-100 shadow-md p-4 ${showIconsOnly ? "" : "min-w-80"}`}
-  >
-    <ul className="space-y-4 w-full">
-      <div className="mb-4 p-3">
-        <Image
-          src={showIconsOnly ? "/favicon.ico" : "/logo.svg"}
-          alt="Logo"
-          width={showIconsOnly ? 24 : 120}
-          height={showIconsOnly ? 24 : 120}
-          priority={true}
-        />
-      </div>
-      <li>
-        {renderLink({
-          path: "/dashboard",
-          icon: isIconFilled("/dashboard") ? BsHouseFill : BsHouse,
-          label: "Home",
-        })}
-      </li>
-      <li>
-        <button
-          className="flex items-center p-3 rounded-md hover:bg-gray-100 w-full"
-          onClick={toggleSearch}
-        >
-          <BsSearch
-            className={`icon ${!showIconsOnly && "mr-3"} text-gray-500 hover:text-cyan-500 text-2xl`}
+}) => {
+  const [profilePath, setProfilePath] = useState<string>("/profile");
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      const session = await getSession();
+      if (session) {
+        const userId = session.user.username;
+        setProfilePath(`/${userId}`);
+      }
+    };
+
+    fetchSession();
+  }, []);
+
+  return (
+    <div
+      className={`fixed left-0 top-0 border-r border-gray-300 h-full flex flex-col bg-gray-100 shadow-md p-4 ${showIconsOnly ? "" : "min-w-80"}`}
+    >
+      <ul className="space-y-4 w-full">
+        <div className="mb-4 p-3">
+          <Image
+            src={showIconsOnly ? "/favicon.ico" : "/logo.svg"}
+            alt="Logo"
+            width={showIconsOnly ? 24 : 120}
+            height={showIconsOnly ? 24 : 120}
+            priority={true}
           />
-          {!showIconsOnly && (
-            <span className="hidden sm:inline-block text-lg">Search</span>
-          )}
-        </button>
-      </li>
-      <li>
-        {renderLink({
-          path: "/create",
-          icon: isIconFilled("/create") ? BsPencilFill : BsPencil,
-          label: "Create",
-        })}
-      </li>
-      <li>
-        {renderLink({
-          path: "/profile",
-          icon: isIconFilled("/profile") ? BsPersonFill : BsPerson,
-          label: "Profile",
-        })}
-      </li>
-    </ul>
-  </div>
-);
+        </div>
+        <li>
+          {renderLink({
+            path: "/dashboard",
+            icon: isIconFilled("/dashboard") ? BsHouseFill : BsHouse,
+            label: "Home",
+          })}
+        </li>
+        <li>
+          <button
+            className="flex items-center p-3 rounded-md hover:bg-gray-100 w-full"
+            onClick={toggleSearch}
+          >
+            <BsSearch
+              className={`icon ${!showIconsOnly && "mr-3"} text-gray-500 hover:text-cyan-500 text-2xl`}
+            />
+            {!showIconsOnly && (
+              <span className="hidden sm:inline-block text-lg">Search</span>
+            )}
+          </button>
+        </li>
+        <li>
+          {renderLink({
+            path: "/create",
+            icon: isIconFilled("/create") ? BsPencilFill : BsPencil,
+            label: "Create",
+          })}
+        </li>
+        <li>
+          {renderLink({
+            path: profilePath,
+            icon: isIconFilled(profilePath) ? BsPersonFill : BsPerson,
+            label: "Profile",
+          })}
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 const BottomIcons = ({
   showIconsOnly,
