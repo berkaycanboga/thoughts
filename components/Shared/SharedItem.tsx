af31b62a99b94fb2021302d496a42c2bea15b3d7
berkay can <berkaycanboga@gmail.com>
2024-02-14 18:47:13 +0300

    refactor: integrated placeholder with SharedItem component

--- components/Shared/SharedItem.tsx
+++ components/Shared/SharedItem.tsx
@@ -12,6 +12,7 @@ import Dropdown from "../Common/Dropdown";
 import LikeCommentItem from "../Like/LikeCommentItem";
 import LikePostItem from "../Like/LikePostItem";
 import DeletePost from "../Post/DeletePost";
+import LoadingPlaceholder from "../Post/PlaceholderPost";
 import UpdatePost from "../Post/UpdatePost";
 
 interface SharedItemProps {
@@ -28,6 +29,7 @@ interface SharedItemProps {
   onPostDelete?: () => void;
   onCommentDelete?: (commentId: number) => void;
   alreadyLink?: boolean;
+  onChildComponentsComplete?: () => void;
 }
 
 const SharedItem = ({
@@ -44,8 +46,9 @@ const SharedItem = ({
   onPostDelete,
   onCommentDelete,
   alreadyLink = false,
+  onChildComponentsComplete,
 }: SharedItemProps) => {
-  const timeAgo = calculateTimeAgo(createdAt, updatedAt);
+  const [childComponentsComplete, setChildComponentsComplete] = useState(false);
   const [owner, setOwner] = useState<number | null>(null);
 
   useEffect(() => {
@@ -65,12 +68,22 @@ const SharedItem = ({
         }
       } catch (error) {
         console.error(`Error fetching ${itemType} details:`, error);
+      } finally {
+        setChildComponentsComplete(true);
       }
     };
 
     fetchOwner();
-  }, [itemType, userId, postId, commentId, updatedAt]);
+  }, [
+    itemType,
+    userId,
+    postId,
+    commentId,
+    updatedAt,
+    onChildComponentsComplete,
+  ]);
 
+  const isLoading = !childComponentsComplete;
   const isOwner = owner === userId;
   const showDropdown = isOwner;
 
@@ -89,7 +102,9 @@ const SharedItem = ({
           <BsDot className="mx-1 text-gray-500" />
           <span className="text-gray-600 text-sm">@{username}</span>
           <BsDot className="mx-1 text-gray-500" />
-          <span className="text-xs text-gray-500">{timeAgo}</span>
+          <span className="text-xs text-gray-500">
+            {calculateTimeAgo(createdAt, updatedAt)}
+          </span>
         </div>
       </div>
       <p className="text-sm mb-1 mt-2 break-words">{content}</p>
@@ -98,63 +113,68 @@ const SharedItem = ({
 
   return (
     <div className="relative">
-      {alreadyLink && itemType === "post" ? (
-        ItemContent
-      ) : itemType === "comment" ? (
-        ItemContent
-      ) : (
-        <Link href={`/${username}/${postId}`}>{ItemContent}</Link>
-      )}
-      {showDropdown && (
-        <div className="absolute top-0 right-0 mt-2 mr-2">
-          <Dropdown
-            trigger={
-              <div className="cursor-pointer flex items-center justify-center text-gray-500 rounded-md text-xl w-7 h-7">
-                <BsThreeDots />
-              </div>
-            }
-          >
+      {isLoading && <LoadingPlaceholder />}
+      {!isLoading && (
+        <>
+          {alreadyLink && itemType === "post" ? (
+            ItemContent
+          ) : itemType === "comment" ? (
+            ItemContent
+          ) : (
+            <Link href={`/${username}/${postId}`}>{ItemContent}</Link>
+          )}
+          {showDropdown && (
+            <div className="absolute top-0 right-0 mt-2 mr-2">
+              <Dropdown
+                trigger={
+                  <div className="cursor-pointer flex items-center justify-center text-gray-500 rounded-md text-xl w-7 h-7">
+                    <BsThreeDots />
+                  </div>
+                }
+              >
+                {itemType === "post" && (
+                  <UpdatePost
+                    userId={userId}
+                    postId={postId}
+                    content={content}
+                    onPostUpdate={onPostUpdate!}
+                  />
+                )}
+                {itemType === "post" && (
+                  <DeletePost
+                    userId={userId}
+                    postId={postId}
+                    onPostDelete={onPostDelete!}
+                  />
+                )}
+                {itemType === "comment" && commentId && (
+                  <DeleteComment
+                    userId={userId}
+                    postId={postId}
+                    commentId={commentId}
+                    onCommentDelete={() => onCommentDelete?.(commentId)}
+                  />
+                )}
+              </Dropdown>
+            </div>
+          )}
+          <div className="absolute bottom-0 right-0 p-1 flex">
             {itemType === "post" && (
-              <UpdatePost
-                userId={userId}
-                postId={postId}
-                content={content}
-                onPostUpdate={onPostUpdate!}
-              />
-            )}
-            {itemType === "post" && (
-              <DeletePost
-                userId={userId}
-                postId={postId}
-                onPostDelete={onPostDelete!}
-              />
+              <LikePostItem userId={userId} postId={postId} />
             )}
             {itemType === "comment" && commentId && (
-              <DeleteComment
+              <LikeCommentItem
                 userId={userId}
                 postId={postId}
                 commentId={commentId}
-                onCommentDelete={() => onCommentDelete?.(commentId)}
               />
             )}
-          </Dropdown>
-        </div>
+            {itemType === "post" && (
+              <CreateCommentFormPopup userId={userId} postId={postId} />
+            )}
+          </div>
+        </>
       )}
-      <div className="absolute bottom-0 right-0 p-1 flex">
-        {itemType === "post" && (
-          <LikePostItem userId={userId} postId={postId} />
-        )}
-        {itemType === "comment" && commentId && (
-          <LikeCommentItem
-            userId={userId}
-            postId={postId}
-            commentId={commentId}
-          />
-        )}
-        {itemType === "post" && (
-          <CreateCommentFormPopup userId={userId} postId={postId} />
-        )}
-      </div>
     </div>
   );
 };
