16ea933fc3b0ce2c07b8a0439c340e78ff5848ea
berkay can <berkaycanboga@gmail.com>
2024-02-14 18:42:03 +0300

    refactor: unified placeholder post style with actual post style

--- components/Post/PlaceholderPost.tsx
+++ components/Post/PlaceholderPost.tsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const LoadingPlaceholder = () => {
   return (
-    <div className="animate-pulse">
-      <div className="flex items-center mb-2">
-        <div className="flex">
-          <div className="w-20 h-4 bg-gray-300 mb-2 mr-2"></div>
-          <div className="w-20 h-4 bg-gray-300 mb-2 mr-2"></div>
-          <div className="w-8 h-4 bg-gray-300 mb-2 mr-2"></div>
+    <div className="p-3 bg-white rounded-md shadow-md h-auto min-h-40 mt-4">
+      <div className="animate-pulse mt-4">
+        <div className="flex items-center mb-2">
+          <div className="flex">
+            <div className="w-20 h-4 bg-gray-300 mb-2 mr-2"></div>
+            <div className="w-20 h-4 bg-gray-300 mb-2 mr-2"></div>
+            <div className="w-8 h-4 bg-gray-300 mb-2 mr-2"></div>
+          </div>
         </div>
+        <div className="w-full h-16 bg-gray-300 mb-2"></div>
       </div>
-      <div className="w-full h-16 bg-gray-300 mb-2"></div>
     </div>
   );
 };
