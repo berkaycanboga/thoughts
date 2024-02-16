2ac4ca61d69f6f94d8dfdd82246b49926fa8fabe
berkay can <berkaycanboga@gmail.com>
2024-02-17 00:26:18 +0300

    fix: fixed baseUrl for deployment

--- utils/api/api.ts
+++ utils/api/api.ts
@@ -1,10 +1,9 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 
 const isLocalhost = process.env.NODE_ENV === "development";
-const vercelUrl = process.env.VERCEL_URL;
 
 const localhostBaseUrl = "http://localhost:3000/api";
-const vercelBaseUrl = `/${vercelUrl}/api`;
+const vercelBaseUrl = "api";
 
 const BASE_URL = isLocalhost ? localhostBaseUrl : vercelBaseUrl;
 
--- utils/decode.ts
+++ utils/decode.ts
@@ -2,10 +2,17 @@ import { NextRequest } from "next/server";
 import { decode } from "next-auth/jwt";
 
 export const decodeToken = async (req: NextRequest) => {
-  const token = req.cookies.get("next-auth.session-token")?.value;
+  const isLocal = process.env.NODE_ENV === "development";
+  const cookieName = isLocal
+    ? "next-auth.session-token"
+    : "__Secure-next-auth.session-token";
+
+  const token = req.cookies.get(cookieName)?.value;
+
   const decodedToken = await decode({
     token: token,
     secret: process.env.NEXTAUTH_SECRET!,
   });
+
   return decodedToken || null;
 };
