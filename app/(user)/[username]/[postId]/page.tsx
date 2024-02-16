66dbb84b4cb6a2b7cd243131db89a6153aaa7bdf
berkay can <berkaycanboga@gmail.com>
2024-02-16 18:11:11 +0300

    fix: ensured not-found display in the username and username/postId routes.

--- app/(user)/[username]/[postId]/page.tsx
+++ app/(user)/[username]/[postId]/page.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 import Post from "../../../../components/Post/Post";
 import { getPostByIdController } from "../../../../controllers/PostController";
+import NotFound from "../../../not-found";
 
 const PostPage = async (ctx: { params: { postId: string } }) => {
   const postId = parseInt(ctx.params.postId, 10);
+
+  if (!postId) {
+    return <NotFound />;
+  }
 
   const userPost = await getPostByIdController(postId);
 
--- app/(user)/[username]/page.tsx
+++ app/(user)/[username]/page.tsx
@@ -2,12 +2,18 @@ import React from "react";
 
 import UserProfile from "../../../components/UserProfile/UserProfile";
 import { getUserIdByUsernameController } from "../../../controllers/UserController";
+import NotFound from "../../not-found";
 
 const UserProfilePage = async (ctx: { params: { username: string } }) => {
   const username = ctx.params.username;
+
   const userId = await getUserIdByUsernameController(username);
 
-  return <UserProfile userId={userId!} />;
+  if (!userId) {
+    return <NotFound />;
+  }
+
+  return <UserProfile userId={userId} />;
 };
 
 export default UserProfilePage;
