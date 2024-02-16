f2a95dca3a5c1d3c9a9e303f02dbd0193396e8d1
berkay can <berkaycanboga@gmail.com>
2024-02-16 18:10:09 +0300

    refactor: removed unnecessary return component statements in signin and signup routes.

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,14 +1,6 @@
-import { getServerSession } from "next-auth/next";
-
 import SignIn from "../../../components/Auth/SignIn/SignIn";
 
 const SignInPage = async () => {
-  const session = getServerSession();
-
-  if (await session) {
-    return <p>You&apos;re already signed in.</p>;
-  }
-
   return (
     <div>
       <SignIn />
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,14 +1,6 @@
-import { getServerSession } from "next-auth/next";
-
 import SignUp from "../../../components/Auth/SignUp/SignUp";
 
 const SignInPage = async () => {
-  const session = getServerSession();
-
-  if (await session) {
-    return <p>You&apos;re already signed in.</p>;
-  }
-
   return (
     <div>
       <SignUp />
