68d362d44935b641ab99a05687791a0393450ee9
berkay can <berkaycanboga@gmail.com>
2024-02-19 00:21:04 +0300

    refactor: simplified time utils

--- utils/time.ts
+++ utils/time.ts
@@ -1,53 +1,44 @@
 import { format, isToday, isYesterday } from "date-fns";
 import { enUS } from "date-fns/locale";
 
-export const calculateTimeAgo = (createdAt: Date, updatedAt?: Date) => {
-  const dateToUse = updatedAt! > createdAt ? updatedAt : createdAt;
-  const isUpdated =
-    updatedAt !== undefined && updatedAt !== null && updatedAt > createdAt;
-
-  if (!(dateToUse instanceof Date)) {
-    console.error("Invalid dateToUse:", dateToUse);
-    return "";
-  }
-
+export const calculateTimeAgo = (date: Date) => {
   const now = new Date();
   const diffInMinutes = Math.floor(
-    (now.getTime() - dateToUse.getTime()) / (60 * 1000),
+    (now.getTime() - date.getTime()) / (60 * 1000),
   );
   const diffInHours = Math.floor(diffInMinutes / 60);
   const diffInDays = Math.floor(diffInHours / 24);
 
   if (diffInMinutes <= 1) {
-    return `just now${isUpdated ? " (Updated)" : ""}`;
+    return `just now`;
   }
 
   if (diffInMinutes < 60) {
-    return `${diffInMinutes}m${isUpdated ? " (Updated)" : ""}`;
+    return `${diffInMinutes}m`;
   }
 
   if (diffInHours < 24) {
-    return `${diffInHours}h${isUpdated ? " (Updated)" : ""}`;
+    return `${diffInHours}h`;
   }
 
   if (diffInDays < 7) {
-    return `${diffInDays}d${isUpdated ? " (Updated)" : ""}`;
+    return `${diffInDays}d`;
   }
 
-  if (isToday(dateToUse)) {
-    return `${format(dateToUse, "h:mm a", { locale: enUS })}${isUpdated ? " (Updated)" : ""}`;
+  if (isToday(date)) {
+    return `${format(date, "h:mm a", { locale: enUS })}`;
   }
 
-  if (isYesterday(dateToUse)) {
-    return `Yesterday${isUpdated ? " (Updated)" : ""}`;
+  if (isYesterday(date)) {
+    return `Yesterday`;
   }
 
   const currentYear = new Date().getFullYear();
-  const postYear = dateToUse.getFullYear();
+  const postYear = date.getFullYear();
 
   if (postYear !== currentYear) {
-    return `${format(dateToUse, "d MMM yyyy", { locale: enUS })}${isUpdated ? " (Updated)" : ""}`;
+    return `${format(date, "d MMM yyyy", { locale: enUS })}`;
   }
 
-  return `${format(dateToUse, "MMM d", { locale: enUS })}${isUpdated ? " (Updated)" : ""}`;
+  return `${format(date, "MMM d", { locale: enUS })}`;
 };
